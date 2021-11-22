1b4b96368002920f33088403de56b3fbed7c3303
apple <apple@appleui-MacBook-Pro.local>
2021-11-22 17:29:43 +0900

    Segues and unwind

--- app-swoosh/app-swoosh/ViewController.swift
+++ app-swoosh/app-swoosh/ViewController.swift
@@ -16,12 +16,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
-        
-        
-        
     }
 
-
+    @IBAction func unwindFromSkillVC(unwindSegue: UIStoryboardSegue) {
+        
+    }
 }
 
